fbed75fe7f8cd236d955110d1061da1afae8ca62
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-19 12:35:27 +0300

    решена 1 задача

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+
+Console.Write("Введите элементы (через пробел): ");
+
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count = count + 1;
+    }
+}
+ 
+Console.WriteLine($"Количество чисел больше нуля = {count}");
